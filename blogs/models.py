e407acd931fafe495273eae40a8b2f7dd597c7a9
Krishna Jindal <rajeshkrjindal@Rajeshs-MacBook-Air.local>
2022-08-28 21:32:44 +0530

    Made some basic changes website is kinda ready

--- blogs/models.py
+++ blogs/models.py
@@ -10,7 +10,7 @@ class Blog(models.Model):
     featured = models.BooleanField(default = False)
 
     def __str__(self):
-        return self.title
+        return self.title + " " + str(self.id)
         
 
 
